794ac0c95e804a9ee4d888657845ae51dc3cb3ba
abhipassgit1 <155081018+abhipassgit1@users.noreply.github.com>
2023-12-29 22:13:44 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 // Give the service worker access to Firebase Messaging.
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging.js')
+importScripts('firebase-app.js')
+importScripts('firebase-messaging.js')
 
 // Initialize the Firebase app in the service worker by passing in the messagingSenderId.
 var config = {
@@ -20,13 +20,3 @@ messaging.setBackgroundMessageHandler(function(payload) {
     return self.registration.showNotification(notificationTitle,
         notificationOptions);
 });
-
-// Handle FCM messages
-    onMessage(messaging, (payload) => {
-      console.log('Received FCM Message:', payload);
-      // Display notification without a service worker
-      const notification = new Notification(payload.notification.title, {
-        body: payload.notification.body,
-        icon: payload.notification.icon,
-      });
-    });
